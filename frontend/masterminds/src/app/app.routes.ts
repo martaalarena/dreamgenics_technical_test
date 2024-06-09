e046a34eb676a8af0ae355db3765acdbbafcb9f3
Marta <martalarena2002@gmail.com>
2024-06-09 17:48:57 +0200

    homePage structure

--- frontend/masterminds/src/app/app.routes.ts
+++ frontend/masterminds/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes, provideRouter } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { GameComponent } from './game/game.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'game', component: GameComponent},
+    {path: '', component: AppComponent}
+];
+
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)]
+  });
